f8ea608113e9eb6c238be7db3b612f052df738b7
mitchellhamilton <mitchell@mitchellhamilton.me>
2017-06-05 23:24:06 +1000

    Add ESLint and Prettier

--- src/index.js
+++ src/index.js
@@ -1,12 +1,142 @@
-var jPath = require('json-path');
-var Queue = require('async-function-queue');
-var extend = require('xtend');
-var equal = require('deep-equal');
+const jPath = require('json-path');
+const Queue = require('async-function-queue');
+const extend = require('xtend');
+const equal = require('deep-equal');
 
-module.exports = createPouchMiddleware;
+/* istanbul ignore next */
+function warn(what) {
+  const fn = console.warn || console.log; // eslint-disable-line no-console
+  if (fn) {
+    fn.call(console, what);
+  }
+}
+
+/* istanbul ignore next */
+function defaultAction(action) {
+  return () => {
+    throw new Error(`no action provided for ${action}`);
+  };
+}
+
+function differences(oldDocs, newDocs) {
+  const result = {
+    new: [],
+    updated: [],
+    deleted: Object.keys(oldDocs).map(oldDocId => oldDocs[oldDocId]),
+  };
+
+  const checkDoc = newDoc => {
+    const id = newDoc._id;
+
+    /* istanbul ignore next */
+    if (!id) {
+      warn('doc with no id');
+    }
+    result.deleted = result.deleted.filter(doc => doc._id !== id);
+    const oldDoc = oldDocs[id];
+    if (!oldDoc) {
+      result.new.push(newDoc);
+    } else if (!equal(oldDoc, newDoc)) {
+      result.updated.push(newDoc);
+    }
+  };
+
+  if (Array.isArray(newDocs)) {
+    newDocs.forEach(doc => {
+      checkDoc(doc);
+    });
+  } else {
+    const keys = Object.keys(newDocs);
+    keys.forEach(key => {
+      checkDoc(newDocs[key]);
+    });
+  }
+
+  return result;
+}
+
+function write(data, responseHandler) {
+  return done => {
+    data.db[data.type](data.doc, (err, resp) => {
+      responseHandler(
+        err,
+        {
+          response: resp,
+          doc: data.doc,
+          type: data.type,
+        },
+        err2 => {
+          done(err2, resp);
+        }
+      );
+    });
+  };
+}
+
+function onDbChange(path, change, dispatch) {
+  const changeDoc = change.doc;
+
+  if (path.changeFilter && !path.changeFilter(changeDoc)) {
+    return;
+  }
+
+  if (changeDoc._deleted) {
+    if (path.docs[changeDoc._id]) {
+      delete path.docs[changeDoc._id];
+      path.propagateDelete(changeDoc, dispatch);
+    }
+  } else {
+    const oldDoc = path.docs[changeDoc._id];
+    path.docs[changeDoc._id] = changeDoc;
+    if (oldDoc) {
+      path.propagateUpdate(changeDoc, dispatch);
+    } else {
+      path.propagateInsert(changeDoc, dispatch);
+    }
+  }
+}
+
+function listen(path, dispatch, initialBatchDispatched) {
+  path.db
+    .allDocs({ include_docs: true })
+    .then(rawAllDocs => {
+      const allDocs = rawAllDocs.rows.map(doc => doc.doc);
+      let filteredAllDocs = allDocs;
+      if (path.changeFilter) {
+        filteredAllDocs = allDocs.filter(path.changeFilter);
+      }
+      filteredAllDocs.forEach(doc => {
+        path.docs[doc._id] = doc;
+      });
+      path.propagateBatchInsert(filteredAllDocs, dispatch);
+      initialBatchDispatched();
+      const changes = path.db.changes({
+        live: true,
+        include_docs: true,
+        since: 'now',
+      });
+      changes.on('change', change => {
+        onDbChange(path, change, dispatch);
+      });
+    })
+    .catch(err => initialBatchDispatched(err));
+}
+
+function processNewStateForPath(path, state) {
+  const pathDocs = jPath.resolve(state, path.path);
+
+  /* istanbul ignore else */
+  if (pathDocs && pathDocs.length) {
+    pathDocs.forEach(docs => {
+      const diffs = differences(path.docs, docs);
+      diffs.new.concat(diffs.updated).forEach(doc => path.insert(doc));
+      diffs.deleted.forEach(doc => path.remove(doc));
+    });
+  }
+}
 
 function createPouchMiddleware(_paths) {
-  var paths = _paths || [];
+  let paths = _paths || [];
   if (!Array.isArray(paths)) {
     paths = [paths];
   }
@@ -15,112 +145,32 @@ function createPouchMiddleware(_paths) {
     throw new Error('PouchMiddleware: no paths');
   }
 
-  var defaultSpec = {
-    path: '.',
-    remove: scheduleRemove,
-    insert: scheduleInsert,
-    propagateDelete,
-    propagateUpdate,
-    propagateInsert,
-    propagateBatchInsert,
-    handleResponse: function(err, data, cb) { cb(err); },
-    queue: Queue(1),
-    docs: {},
-    actions: {
-      remove: defaultAction('remove'),
-      update: defaultAction('update'),
-      insert: defaultAction('insert'),
-      batchInsert: defaultAction('batchInsert'),
-    }
-  }
-
-  paths = paths.map(function(path) {
-    var spec = extend({}, defaultSpec, path);
-    spec.actions = extend({}, defaultSpec.actions, spec.actions);
-    spec.docs = {};
-
-    if (! spec.db) {
-      throw new Error('path ' + path.path + ' needs a db');
-    }
-    return spec;
-  });
-
-  function listen(path, dispatch, initialBatchDispatched) {
-    path.db.allDocs({ include_docs: true }).then((rawAllDocs) => {
-      var allDocs = rawAllDocs.rows.map((doc) => doc.doc);
-      var filteredAllDocs = allDocs;
-      if (path.changeFilter) {
-        filteredAllDocs = allDocs.filter(path.changeFilter);
-      }
-      filteredAllDocs.forEach((doc) => {
-        path.docs[doc._id] = doc;
-      });
-      path.propagateBatchInsert(filteredAllDocs, dispatch);
-      initialBatchDispatched();
-      var changes = path.db.changes({
-        live: true,
-        include_docs: true,
-        since: 'now',
-      });
-      changes.on('change', change => {
-        onDbChange(path, change, dispatch);
-      });
-    }).catch((err) => initialBatchDispatched(err));
-  }
-
-  function processNewStateForPath(path, state) {
-    var docs = jPath.resolve(state, path.path);
-
-    /* istanbul ignore else */
-    if (docs && docs.length) {
-      docs.forEach(function(docs) {
-        var diffs = differences(path.docs, docs);
-        diffs.new.concat(diffs.updated).forEach(doc => path.insert(doc))
-        diffs.deleted.forEach(doc => path.remove(doc));
-      });
-    }
-  }
-
-  function write(data, responseHandler) {
-    return function(done) {
-      data.db[data.type](data.doc, function(err, resp) {
-        responseHandler(
-          err,
-          {
-            response: resp,
-            doc: data.doc,
-            type: data.type
-          },
-          function(err2) {
-            done(err2, resp);
-          }
-        );
-      });
-    };
-  }
-
   function scheduleInsert(doc) {
     this.docs[doc._id] = doc;
-    this.queue.push(write(
-      {
-        type: 'put',
-        doc: doc,
-        db: this.db
-      },
-      this.handleResponse
-    ));
+    this.queue.push(
+      write(
+        {
+          type: 'put',
+          doc,
+          db: this.db,
+        },
+        this.handleResponse
+      )
+    );
   }
 
   function scheduleRemove(doc) {
     delete this.docs[doc._id];
-    this.queue.push(write(
-      {
-        type: 'remove',
-        doc: doc,
-        db: this.db
-      },
-      this.handleResponse
-    ));
+    this.queue.push(
+      write(
+        {
+          type: 'remove',
+          doc,
+          db: this.db,
+        },
+        this.handleResponse
+      )
+    );
   }
 
   function propagateDelete(doc, dispatch) {
@@ -139,100 +189,58 @@ function createPouchMiddleware(_paths) {
     dispatch(this.actions.batchInsert(docs));
   }
 
-  return function(options) {
-    paths.forEach((path) => {
-      listen(path, options.dispatch, (err) => {
+  const defaultSpec = {
+    path: '.',
+    /* eslint-disable no-use-before-define */
+    remove: scheduleRemove,
+    insert: scheduleInsert,
+    propagateDelete,
+    propagateUpdate,
+    propagateInsert,
+    propagateBatchInsert,
+    /* eslint-enable no-use-before-define */
+    handleResponse(err, data, cb) {
+      cb(err);
+    },
+    queue: Queue(1),
+    docs: {},
+    actions: {
+      remove: defaultAction('remove'),
+      update: defaultAction('update'),
+      insert: defaultAction('insert'),
+      batchInsert: defaultAction('batchInsert'),
+    },
+  };
+
+  paths = paths.map(path => {
+    const spec = extend({}, defaultSpec, path);
+    spec.actions = extend({}, defaultSpec.actions, spec.actions);
+    spec.docs = {};
+
+    if (!spec.db) {
+      throw new Error(`path ${path.path} needs a db`);
+    }
+    return spec;
+  });
+
+  return options => {
+    paths.forEach(path => {
+      listen(path, options.dispatch, err => {
         if (path.initialBatchDispatched) {
           path.initialBatchDispatched(err);
         }
       });
     });
 
-    return function(next) {
-      return function(action) {
-        var returnValue = next(action);
-        var newState = options.getState();
+    return next => action => {
+      const returnValue = next(action);
+      const newState = options.getState();
 
-        paths.forEach(path => processNewStateForPath(path, newState));
+      paths.forEach(path => processNewStateForPath(path, newState));
 
-        return returnValue;
-      }
-    }
-  }
-}
-
-function differences(oldDocs, newDocs) {
-  var result = {
-    new: [],
-    updated: [],
-    deleted: Object.keys(oldDocs).map(oldDocId => oldDocs[oldDocId]),
-  };
-
-  var checkDoc = function(newDoc) {
-    var id = newDoc._id;
-
-    /* istanbul ignore next */
-    if (! id) {
-      warn('doc with no id');
-    }
-    result.deleted = result.deleted.filter(doc => doc._id !== id);
-    var oldDoc = oldDocs[id];
-    if (! oldDoc) {
-      result.new.push(newDoc);
-    } else if (!equal(oldDoc, newDoc)) {
-      result.updated.push(newDoc);
-    }
-  };
-
-  if (Array.isArray(newDocs)){
-    newDocs.forEach(function (doc) {
-      checkDoc(doc)
-    });
-  } else{
-    var keys = Object.keys(newDocs);
-    for (var key in newDocs){
-      checkDoc(newDocs[key])
-    }
-  }
-
-
-  return result;
-}
-
-function onDbChange(path, change, dispatch) {
-  var changeDoc = change.doc;
-
-  if(path.changeFilter && (! path.changeFilter(changeDoc))) {
-    return;
-  }
-
-  if (changeDoc._deleted) {
-    if (path.docs[changeDoc._id]) {
-      delete path.docs[changeDoc._id];
-      path.propagateDelete(changeDoc, dispatch);
-    }
-  } else {
-    var oldDoc = path.docs[changeDoc._id];
-    path.docs[changeDoc._id] = changeDoc;
-    if (oldDoc) {
-      path.propagateUpdate(changeDoc, dispatch);
-    } else {
-      path.propagateInsert(changeDoc, dispatch);
-    }
-  }
-}
-
-/* istanbul ignore next */
-function warn(what) {
-  var fn = console.warn || console.log;
-  if (fn) {
-    fn.call(console, what);
-  }
-}
-
-/* istanbul ignore next */
-function defaultAction(action) {
-  return function() {
-    throw new Error('no action provided for ' + action);
+      return returnValue;
+    };
   };
 }
+
+module.exports = createPouchMiddleware;
